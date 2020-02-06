d29607a5f027050e24a09a4f3640b9c79ebc2ef1
Marc Wong <marcw@uci.edu>
2020-02-06 15:20:39 -0800

    handle right align, add test-generation file, add jpeg ignore to gitignore

---
+++ test-generation.js
@@ -0,0 +1,20 @@
+const textToImage = require("./lib/text-to-image");
+
+const testAlign = async (fileName = "test-align.jpeg", textAlign = "left") => {
+  const sampleText = "This is testing alignment";
+
+  try {
+    await textToImage.generate(sampleText, {
+      debug: true,
+      textAlign,
+      fileName,
+      leftMargin: 0
+    });
+  } catch (e) {
+    console.log("error", e);
+  }
+};
+
+testAlign("test-left.jpeg", "left");
+testAlign("test-center.jpeg", "center");
+testAlign("test-right.jpeg", "right");
